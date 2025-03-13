22ee953ef9e339a534e8c8cf0331b1aed4247576
Moe <mchehab20038@gmail.com>
2025-03-13 02:05:11 -0700

    updated

--- src/app/lib/google-oauth.ts
+++ src/app/lib/google-oauth.ts
@@ -1,9 +1,10 @@
 import { google } from "googleapis";
 
+
 //let's exchange the code for an access token
 const oauth2Client = new google.auth.OAuth2(
     process.env.CLIENT_ID,
     process.env.CLIENT_SECRET,
-    process.env.REDIRECT_URI
+    process.env.NODE_ENV === 'development' ? process.env.DEV_REDIRECT_URI : process.env.PROD_REDIRECT_URI
 );
 export default oauth2Client;
